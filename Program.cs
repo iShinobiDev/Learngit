726862a5abd3f8042d23aa5b556df3714382c6db
iShinobiDev <itsabdullahdev@gmail.com>
2022-05-19 09:10:30 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vs_code_c_
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
